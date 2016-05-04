c6e2575c152d4664b77b29c725ad05145d85c0c5
Schwan <JSchwan@email.cz>
2016-05-04 18:26:59 +0200

    Add ConfigurationConverters - Not supported yet
    This is for show project hierarchy
    TODO: complete implementation

---
+++ src/main/java/cz/muni/fi/pb138/log4jtoxml/impl/ConfigurationConverterFromPropToXml.java
@@ -0,0 +1,29 @@
+package cz.muni.fi.pb138.log4jtoxml.impl;
+
+import cz.muni.fi.pb138.log4jtoxml.ConfigurationConverterFromProp;
+import java.io.File;
+
+/**
+ * Implementation of ConfigurationConverter from Prop to XML
+ */
+public class ConfigurationConverterFromPropToXml implements ConfigurationConverterFromProp {
+
+    @Override
+    public boolean validatePropConfiguration(File file) {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void convert(File input) {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void convert(File input, File output) {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ src/main/java/cz/muni/fi/pb138/log4jtoxml/impl/ConfigurationConverterFromXmlToProp.java
@@ -0,0 +1,29 @@
+package cz.muni.fi.pb138.log4jtoxml.impl;
+
+import cz.muni.fi.pb138.log4jtoxml.ConfigurationConverterFromXml;
+import java.io.File;
+
+/**
+ * Implementation of ConfigurationConverter from XML to Prop
+ */
+public class ConfigurationConverterFromXmlToProp implements ConfigurationConverterFromXml {
+
+    @Override
+    public boolean validateXmlConfiguration(File file) {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void convert(File input) {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void convert(File input, File output) {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
