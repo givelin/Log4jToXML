cd7f82df06ff15f7e512191bbb70441e17111ce5
Schwan <JSchwan@email.cz>
2016-06-20 19:54:40 +0200

    Add XMLWriter,

---
+++ src/main/java/cz/muni/fi/pb138/log4jtoxml/constants/XMLConst.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.log4jtoxml.constants;
+
+/**
+ *
+ * @author Jakub
+ */
+public class XMLConst {
+    public static final String CONFIGURATION = "Configuration";
+    
+        //Configuration attributes
+    public static final String PACKAGES ="packages";
+    public static final String STATUS="status";
+    public static final String SCRIPT="strict";
+    public static final String NAME="name";
+    public static final String ADVERTISER="advertiser";
+    public static final String SCHEMA="schema";
+    public static final String[] configAttributes = {PACKAGES,STATUS,SCRIPT,NAME,ADVERTISER,SCHEMA};
+}
--- src/main/java/cz/muni/fi/pb138/log4jtoxml/impl/fileWriters/XMLWriter.java
+++ src/main/java/cz/muni/fi/pb138/log4jtoxml/impl/fileWriters/XMLWriter.java
@@ -5,10 +5,17 @@
  */
 package cz.muni.fi.pb138.log4jtoxml.impl.fileWriters;
 
-import cz.muni.fi.pb138.log4jtoxml.prop.Log4jObject;
+import cz.muni.fi.pb138.log4jtoxml.constants.XMLConst;
 import java.io.File;
 import java.util.Properties;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  *
@@ -18,6 +25,35 @@ public class XMLWriter {
     //class for writing data into file
     
     public static void writeData(File output, Properties properties) {
+        Set<String> propNames = properties.stringPropertyNames();
+            
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        
+        try {
+            builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+            Element mainRootElement = document.createElement(XMLConst.CONFIGURATION);
+            //saying that root element of document is Configuration
+            document.appendChild(mainRootElement);
+            
+            //here set for mainRootElement attributes
+            setConfigurationAttributes(properties, mainRootElement);
+            //set child for main root
+            
+            
+            
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(XMLWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
     
+    private static void setConfigurationAttributes(Properties properties, Element configuration) {
+        for(String attributeName : XMLConst.configAttributes){
+            if(properties.contains(attributeName)) {
+                configuration.setAttribute(attributeName, properties.getProperty(attributeName));
+            }
+        }
     }
 }
